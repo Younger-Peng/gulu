97179d348bc0aadc20154193f132323933dbcd24
PengYang <953413537@qq.com>
2019-02-15 09:45:57 +0800

    添加icon和icon-position

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,62 @@
 <template>
-  <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-  export default {}
+    export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
-  .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-      border-color: var(--border-color-hover);
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        > .icon {
+            order: 1;
+            margin-right: 0.1em;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .content { order: 1; }
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: 0.1em;
+            }
+        }
     }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
-    }
-  }
 </style> 
