5dcda9ac68dd425306a72193b8645cd5f7237cca
PengYang <953413537@qq.com>
2019-02-22 14:55:36 +0800

    button 组件添加3则单元测试

--- src/app.js
+++ src/app.js
@@ -15,106 +15,18 @@ new Vue({
         loading1: false,
         loading2: true,
         loading3: false
+    },
+    created() {
+        setTimeout(() => {
+            const event = new Event('change')
+            const inputEl = this.$el.querySelector('input')
+            inputEl.dispatchEvent(event)
+            console.log('hi')
+        }, 3000)
+    },
+    methods: {
+        inputChange(e) {
+            console.log(e)
+        }
     }
 })
-
-// import chai from 'chai'
-// import spies from 'chai-spies'
-// chai.use(spies)
-// const { expect } = chai
-
-// // 单元测试
-// try {
-//     // 测试元素是否 id为指定的名字 
-//     {
-//         const Constructor = Vue.extend(Button)
-//         const vm = new Constructor({
-//             propsData: {
-//                 icon: 'settings'
-//             }
-//         })
-//         vm.$mount()
-//         let useElement = vm.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-settings')
-//         vm.$el.remove()
-//         vm.$destroy()
-//     }
-
-//     // 测试元素是否 id为指定的名字
-//     {
-//         const Constructor = Vue.extend(Button)
-//         const vm = new Constructor({
-//             propsData: {
-//                 icon: 'settings',
-//                 loading: true
-//             }
-//         })
-//         vm.$mount()
-//         let useElement = vm.$el.querySelector('use')
-//         let href = useElement.getAttribute('xlink:href')
-//         expect(href).to.eq('#i-loading')
-//         vm.$el.remove()
-//         vm.$destroy()
-//     }
-
-//     // 测试元素默认的序号是否为1
-//     {
-//         const div = document.createElement('div')
-//         document.body.appendChild(div)
-//         const Constructor = Vue.extend(Button)
-//         const vm = new Constructor({
-//             propsData: {
-//                 icon: 'settings'
-//             }
-//         })
-//         vm.$mount(div)
-//         let svg = vm.$el.querySelector('svg')
-//         let { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('1')
-//         vm.$el.remove()
-//         vm.$destroy()
-//     }
-
-//     // 测试元素序号是否为2
-//     {
-//         const div = document.createElement('div')
-//         document.body.appendChild(div)
-//         const Constructor = Vue.extend(Button)
-//         const vm = new Constructor({
-//             propsData: {
-//                 icon: 'settings',
-//                 iconPosition: 'right'
-//             }
-//         })
-//         vm.$mount(div)
-//         let svg = vm.$el.querySelector('svg')
-//         const { order } = window.getComputedStyle(svg)
-//         expect(order).to.eq('2')
-//         vm.$el.remove()
-//         vm.$destroy()
-//     }
-
-//     // 测试点击事件是否正常
-//     {
-//         const Constructor = Vue.extend(Button)
-//         const vm = new Constructor({
-//             propsData: {
-//                 icon: 'settings'
-//             }
-//         })
-//         vm.$mount()
-//         const spy = chai.spy(function() {})
-//         vm.$on('click', spy)
-//         const button = vm.$el
-//         button.click()
-//         expect(spy).to.have.been.called()
-//     }
-// } catch(error) {
-//     window.errors = [error]
-// } finally {
-//     window.errors = window.errors || []
-//     window.errors.forEach(error => {
-//         console.error(error.message)
-//     })
-// }
--- test/button.test.js
+++ test/button.test.js
@@ -1,8 +1,6 @@
-import chai from 'chai'
+import { expect } from 'chai'
 import Vue from 'vue'
 import Button from '../src/button'
-
-const { expect } = chai
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
@@ -22,5 +20,55 @@ describe('Button', () => {
         const useElement = vm.$el.querySelector('use')
         expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
         vm.$destroy()
+    })
+
+    it('order 默认是 1', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+
+    it('可以设置 icon 的位置在右侧', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right'
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        const { order } = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+
+    it('点击事件正常', () => {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        vm.$mount()
+
+        const callback = sinon.fake()
+        vm.$on('click', callback)
+        vm.$el.click()
+        expect(callback.callCount).to.eq(1)
     })
 })
