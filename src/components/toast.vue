4e7f2be502356bdf0692dcdb91f707b5c27b2acf
pengyang <953413537@qq.com>
2019-07-30 22:19:06 +0800

    add toast plugin

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,11 +1,61 @@
 <template>
-    <div class="toast">
+    <div class="toast" ref="container">
         <slot></slot>
+        <div class="line" :style="{height: height}"></div>
+        <span v-if="closeButton">{{closeButton.text}}</span>
     </div>
 </template>
 <script type="text/javascript">
     export default {
-        name: 'Toast'
+        name: 'Toast',
+        data() {
+            return {
+                height: 0
+            }
+        },
+        props: {
+            autoClose: {
+                type: Boolean,
+                default: true
+            },
+            autoCloseDelay: {
+                type: Number,
+                default: 5
+            },
+            closeButton: {
+                type: Object,
+                default: () => {
+                    return {
+                        text: 'close',
+                        callback: (toast) => {
+                            toast.close();
+                        }
+                    }
+                }
+            }
+        },
+        mounted() {
+            if (this.autoClose) {
+                setTimeout(() => {
+                    this.close();
+                }, this.autoCloseDelay * 1000)
+            }
+
+            
+            this.$nextTick(() => {
+                const computedStyle = getComputedStyle(this.$refs.container)
+                const height = computedStyle.height
+                this.height = height;
+                console.log(this.height)
+            })
+
+        },
+        methods: {
+            close() {
+                this.$el.remove();
+                this.$destroy();
+            }
+        }
     }
 </script>
 <style lang="scss">
@@ -15,8 +65,12 @@
         left: 50%;
         transform: translateX(-50%);
         background: rgba(0, 0, 0, 0.15);
-        padding: 0.5em 1em;
         color: white;
         border-radius: 0.3em;
+    }
+    .line {
+        display: inline-block;
+        width: 1px;
+        background: red;
     }
 </style>
